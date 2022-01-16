b32c1b4e7e45795a3436ffeb59340b31387b3684
Veikka <32365239+SirVeggie@users.noreply.github.com>
2022-01-16 21:18:34 +0200

    card dragging

--- frontend/src/components/playingCard.tsx
+++ frontend/src/components/playingCard.tsx
@@ -1,29 +1,31 @@
 import style from './playingCard.module.scss'
 import cx from 'classnames'
 import { Card } from "../shared"
-import { CSSProperties, useContext } from 'react'
+import { CSSProperties, useContext, useState } from 'react'
 import { GameContext } from '../context/gameContext'
-import ctool from '../tools/CardTools';
+import ctool from '../tools/CardTools'
 
 type CardProps = {
-    card?: Card
-    fan?: FanValues
-    overrideOnClick?: (card: Card | undefined) => void
-}
+    card?: Card;
+    fan?: FanValues;
+    overrideOnClick?: (card: Card | undefined) => void;
+};
 
 type FaceProps = {
     card: Card,
     isSelected: boolean,
     fan?: FanValues,
-    onClick: () => unknown
-}
+    onClick: () => unknown;
+};
 
 type FanValues = {
     curve: number,
-    distance: number
-}
+    distance: number,
+    size?: number,
+    offset?: number;
+};
 
-export type CardSize = 'large' | 'normal'
+export type CardSize = 'large' | 'normal';
 
 export const PlayingCard = ({ card, fan, overrideOnClick }: CardProps) => {
     const { selectedCard, setSelectedCard } = useContext(GameContext)
@@ -33,15 +35,13 @@ export const PlayingCard = ({ card, fan, overrideOnClick }: CardProps) => {
             overrideOnClick(undefined)
         }
     }
-    
+
     if (!card) {
-        return <div className={cx(style.playingCard)} onClick={backClick}>
-            <CardBack />
-        </div>
+        return <CardBack onClick={backClick} />
     }
 
     const isSelected = selectedCard === card.id
-    
+
     const faceClick = () => {
         if (overrideOnClick) {
             overrideOnClick(card)
@@ -59,22 +59,31 @@ export const PlayingCard = ({ card, fan, overrideOnClick }: CardProps) => {
 }
 
 const CardFace = ({ card, isSelected, fan, onClick }: FaceProps) => {
-    const isRed = ctool.color(card) === 'red';
-    const dist = fan ? fan.distance / Math.sin(fan.curve * (Math.PI/180)) * 2 : undefined
+    const [follow, setFollow] = useState(false)
+
+    const isRed = ctool.color(card) === 'red'
+    const dist = fan ? fan.distance / Math.sin(fan.curve * (Math.PI / 180)) * 2 : undefined
     const inline = fan ? {
         '--angle-amount': `${fan.curve}deg`,
         '--dist': `${dist}px`,
-        '--offset-amount': `${fan.curve / 2}deg`
+        '--offset-amount': fan.offset ? `${fan.offset}deg` : `${fan.curve / 2}deg`,
+        '--size': fan?.size ? `${fan.size}px` : undefined
     } as CSSProperties : undefined
-    
-    return <div className={cx(style.card, isRed && style.red, isSelected && style.selected)} onClick={onClick} style={inline}>
-        <div>{ctool.rankPrefix(card)}</div>
-        <div>{ctool.suitIcon(card)}</div>
-        <div>{ctool.suitIcon(card)}</div>
-        <div>{ctool.suitIcon(card)}</div>
-    </div >
+
+    return (
+        <div className={cx(style.card, fan && style.fan, isRed && style.red, isSelected && style.selected, follow && style.follow)}
+            style={inline}
+            onClick={onClick}
+            onMouseDown={() => setFollow(true)}
+            onMouseUp={() => setFollow(false)}>
+            <div>{ctool.rankPrefix(card)}</div>
+            <div>{ctool.suitIcon(card)}</div>
+            <div>{ctool.suitIcon(card)}</div>
+            <div>{ctool.suitIcon(card)}</div>
+        </div >
+    )
 }
 
-const CardBack = () => {
-    return <div className={cx(style.cardBack)}></div >
+const CardBack = ({ onClick }: { onClick: () => unknown; }) => {
+    return <div className={cx(style.card, style.back)} onClick={onClick} />
 }
