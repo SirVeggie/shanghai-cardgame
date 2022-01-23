583a9a3b4bf00cf9a7e6bed71ff8495a2722fe8b
Eetu Ikonen <eetu.e.ikonen@gmail.com>
2022-01-23 15:40:17 +0200

    Remove builds

--- backend/build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = __importDefault(require("./server"));
-console.log('Started application');
-console.log("Cache all: ", process.env.CACHE_ALL);
-(0, server_1.default)();
