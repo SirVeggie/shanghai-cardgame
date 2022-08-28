902de1c3f7b56366383f34937f7a9e83325bf296
Veikka <32365239+SirVeggie@users.noreply.github.com>
2022-08-28 18:09:48 +0300

    move to cloud server (3)

--- build.sh
+++
@@ -1,7 +0,0 @@
-cd shared
-npm run build
-cd ../backend
-npm run tsc
-cd ../frontend
-npm run build
-cd ..
--- deploy.sh
+++
@@ -1,4 +0,0 @@
-git add -A
-git commit -m "Deploy BUILD commit"
-git push --force
-git push heroku main --force
